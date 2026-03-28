9108d45556e6142bb32bcdf3714453a999ee2ded
Pavel Blinnikov <pcherepaha@yandex.ru>
2026-03-28 13:26:08 +0300

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/PavelBlinnikov/gef-esxi $HOME/gef-esxi
+mkdir -p $HOME/gef-esxi
+wget -q https://raw.githubusercontent.com/PavelBlinnikov/gef-esxi/main/esxi.py -o $HOME/gef-esxi/esxi.py
 
 sed -i -E "s|python sys\.path\.insert\(0, \"/root/\.gef\"\); from gef import \*; Gef\.main\(\)|python sys.path.insert(0, \"/root/.gef\"); sys.path.insert(0, \"$HOME/gef-esxi\"); from gef import *; from esxi import *; Gef.main()|g" /root/.gdbinit
